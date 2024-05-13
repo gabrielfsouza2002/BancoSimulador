88554345cf2d1e33a880da6e7de2bca85de2310e
AGNALDO OLIVEIRA <musichubclub@gmail.com>
2024-05-13 18:48:26 +0000

    Update login.js

--- src/main/resources/frontend/js/login.js
+++ src/main/resources/frontend/js/login.js
@@ -17,9 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     if (mensagemErro) {
-      alert(mensagemErro); // Substitua por uma implementação mais elegante conforme necessário
+      alert(mensagemErro); 
     } else {
-      // Aqui você pode adicionar o código para enviar os dados do formulário
+
       console.log('Formulário válido, enviar dados...');
     }
   });
